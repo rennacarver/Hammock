3049d65246649c42fabd4027f9951ffa7203e0f3
Renna Carver <87547832+rennacarver@users.noreply.github.com>
2024-05-07 22:10:28 -0700

    add connection to MongoDB

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const logger = require('morgan')
 
+require('dotenv').config()
+require('./config/database')
+
 const app = express()
 
 app.use(logger('dev'))
 
-app.use('/', require('./routes/index'))
+app.use('/', require('./Routes/index'))
 
 const port = process.env.PORT || 3001
 
